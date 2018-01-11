8a76600717804da64de8c9419e90684b3e14aa5d
Paul <Paul@MagentoPro.co.uk>
2018-01-11 10:13:52 +0000

    Release Version 1.0.0 Fixed Blank Config Page

--- demo.reportmapper.com/app/code/community/ReportMapper/SalesReportMaps/Helper/Data.php
+++ demo.reportmapper.com/app/code/community/ReportMapper/SalesReportMaps/Helper/Data.php
@@ -1,31 +1,18 @@
 <?php
+
 /**
- * Magento Community Edition
- *
- * NOTICE OF LICENSE
- *
- * This source file is subject to the Open Software License (OSL 3.0)
- * that is bundled with this package in the file LICENSE.txt.
- * It is also available through the world-wide-web at this URL:
- * http://opensource.org/licenses/osl-3.0.php
- * If you did not receive a copy of the license and are unable to
- * obtain it through the world-wide-web, please send an email
- * to license@magentocommerce.com so we can send you a copy immediately.
- *
- * DISCLAIMER
- *
- * Do not edit or add to this file if you wish to upgrade Magento to newer
- * versions in the future. If you wish to customize Magento for your
- * needs please refer to http://www.magentocommerce.com for more information.
- *
- * @category    RM
- * @package     ReportMappe_SalesReportMaps
- * @created     10th January 2018 3.00pm
- * @author      ReportMapper magento team <support@reportmapper.com>
- * @purpose     ReportMapper block
- * @copyright   Copyright (c) 2018 ReportMapper.com
- * @license     http://opensource.org/licenses/osl-3.0.php  Open Software License
+ * Class ReportMapper_SalesReportMaps_Helper_Data
+ * ReportMapper - Sales and Marketing Reporting Tool for Magento 1x
+ * @category ReportMapper
+ * @package	ReportMapper_SalesReportMaps
+ * @version	1.0.0
+ * @created	10th January 2018 3.00pm
+ * @author		Paul Watt <support@reportmapper.com>
+ * @purpose	ReportMapper block
+ * @copyright	Copyright (c) 2018 ReportMapper.com
+ * @license	http://opensource.org/licenses/osl-3.0.php  Open Software License
  */
+
 class ReportMapper_SalesReportMaps_Helper_Data
     extends Mage_Core_Helper_Abstract
 {
